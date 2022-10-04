5e8d1d49f78222760892b94784e465b8138d1309
feyza ışık <feyza_43_4335@hotmail.com>
2022-10-04 14:35:19 +0300

    Add project files.

---
+++ Helper/ConnectionHelper.cs
@@ -0,0 +1,24 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Helper
+{
+    //IConnection interface’ini kullandığımız için ConnectionHelper’ın Oracle ve MsSQL sınıfları hakkında bilgi sahibi olmasını engellemiş olduk.
+    
+    public class ConnectionHelper
+    {
+        private IConnection _connection;
+        public ConnectionHelper(IConnection connection)
+        {
+            _connection = connection;
+        }
+        public string GetData()
+        {
+            return _connection.GetData();
+        }
+    }
+}
---
+++ IocOrnek/Controllers/ValuesController.cs
@@ -0,0 +1,30 @@
+﻿using DAL;
+using Helper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace IocOrnek.Controllers
+{
+    //Örneğimizi .NET mvc’de yaptığımız ve .NET mvc içerisinde de hali hazırda bir dependency injection
+    //mekanizması bulunduğu için ekstra bir kütüphane kurmamıza gerek kalmadı.
+    public class ValuesController : Controller
+    {
+        private readonly IConnection _connection;
+        public ValuesController(IConnection connection)
+        {
+            _connection = connection;
+        }
+        [HttpGet]
+        public ActionResult Get()
+        {
+            var getdata= _connection.GetData();
+            return View(getdata);
+        }
+    }
+
+   
+}
